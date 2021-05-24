60b1ff29a4f5c0dc66660bf189bb650d48d40f7b
Zuzukin-dp <zuzukinz@gmail.com>
2021-05-24 22:42:32 +0300

    add flake8, travis ci

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    create = models.DateTimeField(auto_now_add=True)
+    # source = models.CharField(max_length=64)
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from currency.utils import generate_password as gp, read_txt
 

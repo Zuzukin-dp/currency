724f02292245a1285b24eb8fc47143fa96f7406d
Zuzukin-dp <zuzukinz@gmail.com>
2021-05-25 01:21:12 +0300

    on models Rate rename fild type to cur_type (flake8)

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Rate(models.Model):
-    type = models.CharField(max_length=5)
+    cur_type = models.CharField(max_length=5)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)

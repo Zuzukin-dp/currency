4263cb7d5f15e2fc82f41b03066ab75914844881
Zuzukin-dp <zuzukinz@gmail.com>
2021-05-25 00:09:14 +0300

    add models Rate

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,5 +5,5 @@ class Rate(models.Model):
     type = models.CharField(max_length=5)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
-    create = models.DateTimeField(auto_now_add=True)
-    # source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=64)

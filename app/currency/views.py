c16007218579b30dcdf52fd9f18476a871f83356
Zuzukin-dp <zuzukinz@gmail.com>
2021-05-25 01:47:45 +0300

    test travis

--- app/currency/views.py
+++ app/currency/views.py
@@ -3,6 +3,11 @@ from currency.utils import generate_password as gp, read_txt
 from django.http import HttpResponse
 # from django.shortcuts import render
 
+#########Rdjwdghwefhwv w l ur
+
+
+
+
 
 def generate_pass(request):
     password = gp()

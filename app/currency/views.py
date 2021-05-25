134da666233b5c7d1e31d281529aec8ff6026120
Zuzukin-dp <zuzukinz@gmail.com>
2021-05-25 20:36:40 +0300

    flake8 fix

--- app/currency/views.py
+++ app/currency/views.py
@@ -3,11 +3,6 @@ from currency.utils import generate_password as gp, read_txt
 from django.http import HttpResponse
 # from django.shortcuts import render
 
-#########Rdjwdghwefhwv w l ur
-
-
-
-
 
 def generate_pass(request):
     password = gp()

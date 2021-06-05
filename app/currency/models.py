122f9c915d32abe88f57955cdecc4ce61c4d70d2
Zuzukin-dp <zuzukinz@gmail.com>
2021-06-05 21:57:51 +0300

    ad model Source

--- app/currency/models.py
+++ app/currency/models.py
@@ -13,3 +13,11 @@ class ContactUs(models.Model):
     email_from = models.EmailField(max_length=128)
     subject = models.CharField(max_length=12)
     message = models.TextField()
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=12)
+    url = models.URLField(max_length=255)
+    phone = models.CharField(max_length=13)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)

d5d199d169adb22b84d1538b320117fb2deee0ce
Zuzukin-dp <zuzukinz@gmail.com>
2021-06-15 22:42:15 +0300

    update Source model

--- app/currency/models.py
+++ app/currency/models.py
@@ -19,6 +19,6 @@ class ContactUs(models.Model):
 class Source(models.Model):
     name = models.CharField(max_length=12)
     url = models.URLField(max_length=255)
-    phone = models.CharField(max_length=13)
+    phone = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)

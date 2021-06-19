15354662750c0d8bfce82312f36567c7609a31b3
Zuzukin-dp <zuzukinz@gmail.com>
2021-06-19 23:14:06 +0300

    add name admin

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -7,7 +7,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', index, name='index'),
     path('currency/', include('currency.urls')),
     path('__debug__/', include(debug_toolbar.urls)),

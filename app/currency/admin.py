e751658365af21376641f382b3c3c6f79b31ea2b
Zuzukin-dp <zuzukinz@gmail.com>
2021-06-20 09:44:41 +0300

    add has_change_permission

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -100,18 +100,14 @@ class ContactUsAdmin(ImportExportModelAdmin):
     search_fields = (
         'email_from',
     )
-    readonly_fields = (
-        'id',
-        'email_from',
-        'subject',
-        'message',
-        'created',
-    )
 
     def has_delete_permission(self, request, obj=None):
         return False
 
     def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
         return False
 
 

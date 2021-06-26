21a043092fbac9e978096f3d4be2f296e820e33e
Zuzukin-dp <zuzukinz@gmail.com>
2021-06-26 17:48:59 +0300

    add CELERY_BEAT_SCHEDULE

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from celery.schedules import crontab
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,6 +142,14 @@ INTERNAL_IPS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 CELERY_BROKER_URL = 'amqp://localhost'
+
+
+CELERY_BEAT_SCHEDULE = {
+    'privatbank': {
+        'task': 'currency.tasks.print_beats',
+        'schedule': crontab(minute='*/1'),
+    },
+}
 
 try:
     from settings.settings_local import *  # noqa

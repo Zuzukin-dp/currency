20b64c386ef2304950dd385844d0d50b7c265d46
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-07 22:42:08 +0300

    fix Rate

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,17 +1,9 @@
 from django.db import models
+from currency import choices
 
 
 class Rate(models.Model):
-
-    RATE_TYPE_USD = 10
-    RATE_TYPE_EUR = 11
-
-    RATE_TYPE_CHOICES = (
-        (RATE_TYPE_USD, 'Dollar'),
-        (RATE_TYPE_EUR, 'Euro'),
-    )
-
-    cur_type = models.PositiveSmallIntegerField(choices=RATE_TYPE_CHOICES)
+    cur_type = models.PositiveSmallIntegerField(choices=choices.RATE_TYPE_CHOICES)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)

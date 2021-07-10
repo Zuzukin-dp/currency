b91ce0f42ff8aed9bf84368f4e79281665e3f5da
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-10 23:03:58 +0300

    add AnalyticsMiddleware

---
+++ app/currency/middlewares.py
@@ -0,0 +1,33 @@
+from django.db.models import F
+
+from currency.models import Analytics
+from currency import choices
+
+
+class AnalyticsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        # numerator = Analytics.objects.filter(request_method=request_method, path=request.path,
+        #                                      status=self.get_response(request).status_code).last()
+        # if numerator:
+        #     numerator.counter += 1
+        #     numerator.save()
+        # else:
+        #     Analytics.objects.create(request_method=request_method, path=request.path,
+        #                              counter=1, status=self.get_response(request).status_code)
+
+        response = self.get_response(request)
+        request_method = choices.REQUEST_METHOD_CHOICES_MAPPER[request.method]
+
+        obj, created = Analytics.objects.get_or_create(
+            request_method=request_method, path=request.path, status=response.status_code,
+            defaults={'counter': 1}
+        )
+        if not created:
+            Analytics.objects.filter(pk=obj.pk).update(counter=F('counter') + 1)
+        # breakpoint()
+
+        return response

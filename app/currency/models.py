e403e4a7983639ac510b3de8da81926c414d5746
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-13 22:22:26 +0300

    alter Rate, Source models

--- app/currency/models.py
+++ app/currency/models.py
@@ -18,7 +18,6 @@ class Rate(models.Model):
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
-    source = models.CharField(max_length=64)
     bank = models.ForeignKey(
         Source,
         on_delete=models.CASCADE,

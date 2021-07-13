2d938828c39ccd0f06b2b4a18a3f0b532da1df10
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-13 21:15:33 +0300

    add consts

---
+++ app/currency/consts.py
@@ -0,0 +1,6 @@
+CODE_NAME_PRIVATBANK = 'privatbank'
+CODE_NAME_MONOBANK = 'monobank'
+CODE_NAME_VKURSE = 'vkurse.dp.ua'
+CODE_NAME_OSCHADBANK = 'oschadbank'
+CODE_NAME_ALFABANK = 'alfabank'
+CODE_NAME_RAIFFEISEN = 'raiffeisen'

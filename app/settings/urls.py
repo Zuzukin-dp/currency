fbc538fc435ac99cd43d4958dbd331644e40e620
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-18 22:48:14 +0300

    add path auth

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -9,6 +9,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', index, name='index'),
+    path('auth/', include('django.contrib.auth.urls')),
     path('currency/', include('currency.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]

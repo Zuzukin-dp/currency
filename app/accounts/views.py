5bf3de67e5c83da96a06000c95e7abc2177d9d5d
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-19 21:33:07 +0300

    created view MyProfile

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from accounts.models import User
+
+
+class MyProfile(LoginRequiredMixin, UpdateView):
+    queryset = User.objects.all()
+    template_name = 'my-profile.html'
+    success_url = reverse_lazy('index')
+    fields = (
+        'first_name',
+        'last_name',
+    )
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(pk=self.request.user.pk)
+    #     return queryset
+
+    def get_object(self, queryset=None):
+        return self.request.user

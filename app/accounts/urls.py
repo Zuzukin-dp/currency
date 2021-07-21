0dbc9d2bdea80c0a12d440b20dd8e27814736223
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-21 20:30:49 +0300

    fix password reset

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,6 +1,5 @@
 from accounts import views
 
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 app_name = 'accounts'
@@ -8,9 +7,4 @@ app_name = 'accounts'
 urlpatterns = [
     # path('my-profile/<int:pk>', views.MyProfile.as_view(), name='my-profile'),
     path('my-profile/', views.MyProfile.as_view(), name='my-profile'),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

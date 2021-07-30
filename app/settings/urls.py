a4c95b76e4a6ec338cc27e41ac54881993a0559c
Zuzukin-dp <zuzukinz@gmail.com>
2021-07-31 00:48:53 +0300

    flake8 fix

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,12 +1,11 @@
 from currency.views import index
-from django.conf.urls.static import static
 
 import debug_toolbar
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
